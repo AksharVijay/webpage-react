7f23e8be0d8804102f3333efb15ece236d3c5343
aksharvijay <aksharvij@gmail.com>
2020-09-28 12:24:47 +1300

    Updates

---
+++ precache-manifest.9752c70ad7cdd342fcc30c7df8851f6d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "14888d7dd6e431d1d303ef5684655231",
+    "url": "/webpage-react/index.html"
+  },
+  {
+    "revision": "33f8dad32fd6578f471d",
+    "url": "/webpage-react/static/css/main.f102ea17.chunk.css"
+  },
+  {
+    "revision": "7992c1eb86b0d9fa5edc",
+    "url": "/webpage-react/static/js/2.69e1ea1c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/webpage-react/static/js/2.69e1ea1c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "33f8dad32fd6578f471d",
+    "url": "/webpage-react/static/js/main.cb732861.chunk.js"
+  },
+  {
+    "revision": "03ad0919af317d01d292",
+    "url": "/webpage-react/static/js/runtime-main.b3e148d9.js"
+  }
+]);
